2a1d56bdb2048bbda76829e59c8af9290152d5db
robbittim <robbittim@gmail.com>
2021-04-21 21:50:25 -0500

    LC-37 Leetcode everyday
    206. Reverse Linked List

---
+++ src/com/linkedList/ReverseLinkedList_206.java
@@ -0,0 +1,33 @@
+package com.linkedList;
+
+/*
+206. Reverse Linked List
+
+https://leetcode.com/problems/reverse-linked-list/
+ */
+public class ReverseLinkedList_206 {
+
+    public ListNode reverseList(ListNode head) {
+
+        if(head == null ) return head;
+
+        ListNode temp = null;
+
+        ListNode curr = head;
+
+         while(curr !=null){
+             ListNode next = curr.next;
+             curr.next = temp;
+             temp = curr;
+             curr = next;
+
+         }
+
+        return temp;
+
+
+
+
+    }
+
+}
